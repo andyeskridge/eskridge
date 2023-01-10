955207b19dff2d7fc912eeaf173638b6d48f4fe9
Andy Eskridge <andy.eskridge@gmail.com>
2023-01-10 09:38:52 -0600

    Turbo (#115)
    * wip turbo
    
    * update turbo
    
    * finally got caching working
    
    * remove .logs
    
    * ignore the folder
    
    * turbo the package scripts
    
    * stop the recursion
    
    * updated .gitignore

--- prettier.config.js
+++ prettier.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   singleQuote: true,
   semi: false,
-  plugins: [
-    require('@trivago/prettier-plugin-sort-imports'),
-    require('prettier-plugin-tailwindcss'),
-  ],
-  importOrder: ['^@components/(.*)$', '^@lib/(.*)$', '^@images/(.*)$', '^[./]'],
-  importOrderSeparation: true,
+  plugins: [require('prettier-plugin-tailwindcss')],
 }
