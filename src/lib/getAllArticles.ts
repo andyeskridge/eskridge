1e23a65216df32ce4db99a8240611857f9f8a8e7
Andy Eskridge <andy.eskridge@gmail.com>
2023-08-07 12:39:45 -0500

    fresh start

--- src/lib/getAllArticles.ts
+++
@@ -1,7 +0,0 @@
-import { allArticles } from 'contentlayer/generated'
-
-export function getAllArticles() {
-  return allArticles.sort(
-    (a, z) => new Date(z.date).valueOf() - new Date(a.date).valueOf(),
-  )
-}
