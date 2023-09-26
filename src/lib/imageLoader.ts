613442eaca421b469488682b8f83dc5ecd832bee
Andy Eskridge <andy.eskridge@gmail.com>
2023-09-26 16:59:11 -0500

    add cloudflare loader (#429)

---
+++ src/lib/imageLoader.ts
@@ -0,0 +1,22 @@
+const normalizeSrc = (src: string) => {
+  return src.startsWith('/') ? src.slice(1) : src
+}
+
+export default function cloudflareLoader({
+  src,
+  width,
+  quality,
+}: {
+  src: string
+  width: number
+  quality?: number
+}) {
+  const params = [`width=${width}`]
+  if (quality) {
+    params.push(`quality=${quality}`)
+  }
+  const paramsString = params.join(',')
+  return `https://eskridge.dev/cdn-cgi/image/${paramsString}/${normalizeSrc(
+    src,
+  )}`
+}
