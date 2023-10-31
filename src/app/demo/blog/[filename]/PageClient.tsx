ce37ecdb36562f3c2006128f6f574dc5a085c233
Andy Eskridge <andy.eskridge@gmail.com>
2023-10-30 22:24:44 -0500

    add in demo tina route, update deps

---
+++ src/app/demo/blog/[filename]/PageClient.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { useTina } from 'tinacms/dist/react'
+import PageServer from './PageServer'
+
+export default function PageClient({
+  query,
+  variables,
+  data,
+}: {
+  query: any
+  variables: any
+  data: any
+}) {
+  const { data: tinaData } = useTina({
+    query: query,
+    variables: variables,
+    data: data,
+  })
+
+  return (
+    <>
+      <div className="m-6 inline-block bg-red-100 p-6 uppercase text-slate-900">
+        This is rendered on the Client
+      </div>
+      <PageServer data={tinaData} />
+    </>
+  )
+}
