f891fc8dacae8454ace4b2697741ae52ef023004
Andy Eskridge <andy.eskridge@gmail.com>
2023-12-15 08:11:32 -0600

    add exit route

---
+++ src/app/api/preview/exit/route.ts
@@ -0,0 +1,23 @@
+// route handler with secret and slug
+import { draftMode } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+export const runtime = 'edge'
+
+export async function GET(request: Request) {
+  // Parse query string parameters
+  const { searchParams } = new URL(request.url)
+  const slug = searchParams.get('slug')
+
+  if (slug === null) {
+    return new Response('Slug is not defined', { status: 401 })
+  }
+
+  // Enter preview mode in local development
+  // Enable Draft Mode by setting the cookie
+  draftMode().disable()
+
+  // Redirect to the path from the fetched post
+  // We don't redirect to searchParams.slug as that might lead to open redirect vulnerabilities
+  redirect(slug)
+}
