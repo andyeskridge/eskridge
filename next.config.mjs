de83e7e89a7bd6f6a4e4a713b7e15189564fa6d2
Andy Eskridge <andy.eskridge@gmail.com>
2023-12-15 10:23:32 -0600

    allow tina assets

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['ts', 'tsx'],
-  reactStrictMode: true,
   images: {
     loader: 'custom',
     loaderFile: './src/lib/imageLoader.ts',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'assets.tina.io',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 }
 
