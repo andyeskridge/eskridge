37b96ecca57757d05b5c7462fe86406e3f15ed41
Andy Eskridge <andy.eskridge@gmail.com>
2024-01-02 21:41:59 -0600

    only use main as a branch

--- tina/config.ts
+++ tina/config.ts
@@ -2,12 +2,7 @@ import { defineConfig } from 'tinacms'
 import { TokenObject } from 'tinacms/dist/auth/authenticate'
 
 // Your hosting provider likely exposes this as an environment variable
-const branch =
-  process.env.CF_PAGES_BRANCH ||
-  process.env.HEAD ||
-  process.env.VERCEL_GIT_COMMIT_REF ||
-  process.env.GITHUB_REF_NAME ||
-  'main'
+const branch = 'main'
 
 export default defineConfig({
   branch,
