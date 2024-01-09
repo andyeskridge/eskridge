9cf4779baa0331fe20e40abfed6373f1298718a3
Andy Eskridge <andy.eskridge@gmail.com>
2024-01-09 09:22:58 -0500

    Update page.tsx

--- src/app/articles/[filename]/page.tsx
+++ src/app/articles/[filename]/page.tsx
@@ -2,6 +2,7 @@ import client from '@/tina/__generated__/client'
 import PageClient from './PageClient'
 
 export const runtime = 'edge'
+export const dynamicParams = false;
 
 export default async function Page({
   params: { filename },
