6177dfcf0b836921833aa15fe6912ca4bf33ae5a
Andy Eskridge <andy.eskridge@gmail.com>
2024-08-01 17:26:21 -0500

    add admin link

--- src/app/articles/[filename]/PageServer.tsx
+++ src/app/articles/[filename]/PageServer.tsx
@@ -4,7 +4,6 @@ import { ArticleLayout } from '@/components/ArticleLayout'
 export default function PageServer({ data }: { data: any }) {
   return (
     <ArticleLayout article={data.post} isRssFeed={false}>
-      <h1>{data.post.title}</h1>
       <TinaMarkdown content={data.post.body} />
     </ArticleLayout>
   )
