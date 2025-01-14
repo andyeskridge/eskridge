ef968c30bee7acc1a51eb517f801c4192520294e
Andy Eskridge <andy.eskridge@gmail.com>
2025-01-14 11:09:43 -0600

    update image loader

--- src/lib/imageLoader.ts
+++ src/lib/imageLoader.ts
@@ -1,3 +1,5 @@
+'use client'
+
 const normalizeSrc = (src: string) => {
   return src.startsWith('/') ? src.slice(1) : src
 }
@@ -11,12 +13,6 @@ export default function cloudflareLoader({
   width: number
   quality?: number
 }) {
-  const params = [`width=${width}`]
-  if (quality) {
-    params.push(`quality=${quality}`)
-  }
-  const paramsString = params.join(',')
-  return `https://eskridge.dev/cdn-cgi/image/${paramsString}/${normalizeSrc(
-    src,
-  )}`
+  const params = [`width=${width}`, `quality=${quality || 75}`, 'format=auto']
+  return `https://eskridge.dev/cdn-cgi/image/${params.join(',')}/${normalizeSrc(src)}`
 }
