a7fefd5b76eec944f73e4828f3a9ac99cf24c75b
Andy Eskridge <andy.eskridge@gmail.com>
2025-01-14 11:07:40 -0600

    prettier

--- src/app/articles/[filename]/page.tsx
+++ src/app/articles/[filename]/page.tsx
@@ -4,16 +4,12 @@ import PageClient from './PageClient'
 
 export const dynamicParams = false
 
-export default async function Page(
-  props: {
-    params: Promise<{ filename: string }>
-  }
-) {
-  const params = await props.params;
+export default async function Page(props: {
+  params: Promise<{ filename: string }>
+}) {
+  const params = await props.params
 
-  const {
-    filename
-  } = params;
+  const { filename } = params
 
   const res = await client.queries.post({
     relativePath: `${filename}.md`,
