12abcd69b86d815d93b7ab8fc0873af6f99ea0f7
Andy Eskridge <andy.eskridge@gmail.com>
2025-02-12 11:56:48 -0600

    add the queue

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,15 +1,16 @@
 import type { OpenNextConfig } from '@opennextjs/aws/types/open-next.js';
-import cache from '@opennextjs/cloudflare/kvCache';
+import cache from '@opennextjs/cloudflare/kv-cache';
+import memoryQueue from '@opennextjs/cloudflare/memory-queue';
 
 const config: OpenNextConfig = {
   default: {
     override: {
       wrapper: 'cloudflare-node',
       converter: 'edge',
-      // set `incrementalCache` to "dummy" to disable KV cache
       incrementalCache: async () => cache,
+      queue: async () => memoryQueue,
+      // Unused implementation
       tagCache: 'dummy',
-      queue: 'dummy',
     },
   },
 
