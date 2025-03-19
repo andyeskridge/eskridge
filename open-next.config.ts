3ec2994d8f3481c8803b40134a23eedb0fdef563
Andy Eskridge <andy.eskridge@gmail.com>
2025-03-19 14:12:57 -0500

    add tag cache

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,9 +1,11 @@
 import { defineCloudflareConfig } from '@opennextjs/cloudflare';
+import d1TagCache from '@opennextjs/cloudflare/d1-tag-cache';
 import cache from '@opennextjs/cloudflare/kv-cache';
 import memoryQueue from '@opennextjs/cloudflare/memory-queue';
 
 const config = defineCloudflareConfig({
   incrementalCache: cache,
+  tagCache: d1TagCache,
   queue: memoryQueue,
 });
 
