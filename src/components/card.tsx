808fe0c55cd51b51509db4367ced64294b3c7858
Andy Eskridge <andy.eskridge@gmail.com>
2025-12-15 13:37:53 -0600

    chore(deps): update schema version to 2.3.9 and improve class names in components

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -30,9 +30,9 @@ Card.Link = function CardLink({
 }: ComponentPropsWithoutRef<typeof Link>) {
   return (
     <>
-      <div className="-inset-x-4 -inset-y-6 sm:-inset-x-6 absolute z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:rounded-2xl dark:bg-zinc-800/50" />
+      <div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:-inset-x-6 sm:rounded-2xl dark:bg-zinc-800/50" />
       <Link {...props}>
-        <span className="-inset-x-4 -inset-y-6 sm:-inset-x-6 absolute z-20 sm:rounded-2xl" />
+        <span className="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl" />
         <span className="relative z-10">{children}</span>
       </Link>
     </>
